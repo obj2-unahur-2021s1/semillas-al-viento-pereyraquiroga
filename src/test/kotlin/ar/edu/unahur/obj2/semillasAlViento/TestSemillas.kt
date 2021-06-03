ebf82caeb8e2806c7af62eca39e928e8f031a824
Lau38 <lautiaguii@gmail.com>
2021-06-03 19:19:31 -0300

    test menta y soja

--- src/test/kotlin/ar/edu/unahur/obj2/semillasAlViento/TestSemillas.kt
+++ src/test/kotlin/ar/edu/unahur/obj2/semillasAlViento/TestSemillas.kt
@@ -5,11 +5,15 @@ import io.kotest.matchers.shouldBe
 
 class SemillasTest : DescribeSpec({
 
-    var planta1 = Menta(2021, 0.5F)
+    var plantaMenta = Menta(2021, 0.5F)
+    var plantaSoja= Soja(2020, 0.6F,true)
 
     describe("Horas de sol que tolera"){
         it("planta de Menta"){
-            planta1.horasDeSolQueTolera().shouldBe(6)
+            plantaMenta.horasDeSolQueTolera().shouldBe(6)
+        }
+        it("planta de Soja"){
+            plantaSoja.horasDeSolQueTolera().shouldBe(7)
         }
     }
 })
